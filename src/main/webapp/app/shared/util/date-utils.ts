06e30639a50b5bdbd01ccbde38d4aa49c858f99a
Vladimir Livin <vladimir@livesystems.ru>
2023-12-13 01:41:45 +0300

    Add current time when creating new reservation.

--- src/main/webapp/app/shared/util/date-utils.ts
+++ src/main/webapp/app/shared/util/date-utils.ts
@@ -6,4 +6,4 @@ export const convertDateTimeFromServer = date => (date ? dayjs(date).format(APP_
 
 export const convertDateTimeToServer = (date?: string): dayjs.Dayjs | null => (date ? dayjs(date) : null);
 
-export const displayDefaultDateTime = () => dayjs().startOf('day').format(APP_LOCAL_DATETIME_FORMAT);
+export const displayDefaultDateTime = () => dayjs().format(APP_LOCAL_DATETIME_FORMAT);
