01ecca0f0b8c87343f04a023b5dd43327d587422
Vladimir Livin <vladimir@livesystems.ru>
2023-12-18 15:02:11 +0300

    Add inventory availability check support tests.

--- src/test/java/com/getinventory/domain/InventoryTest.java
+++ src/test/java/com/getinventory/domain/InventoryTest.java
@@ -1,8 +1,9 @@
 package com.getinventory.domain;
 
 import static com.getinventory.domain.InventoryTestSamples.*;
-import static com.getinventory.domain.ReservationTestSamples.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.getinventory.web.rest.TestUtil;
 import org.junit.jupiter.api.Test;
@@ -12,28 +13,24 @@ class InventoryTest {
     @Test
     void equalsVerifier() throws Exception {
         TestUtil.equalsVerifier(Inventory.class);
-        Inventory inventory1 = getInventorySample1();
+        Inventory inventory1 = getInventorySample();
         Inventory inventory2 = new Inventory();
         assertThat(inventory1).isNotEqualTo(inventory2);
 
         inventory2.setId(inventory1.getId());
         assertThat(inventory1).isEqualTo(inventory2);
 
-        inventory2 = getInventorySample2();
+        inventory2 = getReservedInventorySample();
         assertThat(inventory1).isNotEqualTo(inventory2);
     }
 
     @Test
-    void reservationTest() throws Exception {
-        Inventory inventory = getInventoryRandomSampleGenerator();
-        Reservation reservationBack = getReservationRandomSampleGenerator();
+    void inventory_whenHasNoReservation_shouldBeAvailable() {
+        assertTrue(getInventorySample().isAvailable());
+    }
 
-        inventory.setReservation(reservationBack);
-        assertThat(inventory.getReservation()).isEqualTo(reservationBack);
-        assertThat(reservationBack.getInventory()).isEqualTo(inventory);
-
-        inventory.reservation(null);
-        assertThat(inventory.getReservation()).isNull();
-        assertThat(reservationBack.getInventory()).isNull();
+    @Test
+    void inventory_withReservation_shouldBeNotAvailable() {
+        assertFalse(getReservedInventorySample().isAvailable());
     }
 }
--- src/test/java/com/getinventory/domain/InventoryTestSamples.java
+++ src/test/java/com/getinventory/domain/InventoryTestSamples.java
@@ -1,5 +1,6 @@
 package com.getinventory.domain;
 
+import java.time.Instant;
 import java.util.Random;
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicLong;
@@ -9,12 +10,15 @@ public class InventoryTestSamples {
     private static final Random random = new Random();
     private static final AtomicLong longCount = new AtomicLong(random.nextInt() + (2 * Integer.MAX_VALUE));
 
-    public static Inventory getInventorySample1() {
+    public static Inventory getInventorySample() {
         return new Inventory().id(1L).name("name1");
     }
 
-    public static Inventory getInventorySample2() {
-        return new Inventory().id(2L).name("name2");
+    public static Inventory getReservedInventorySample() {
+        return new Inventory()
+            .id(2L)
+            .name("name2")
+            .reservation(new Reservation().id(110L).reservedAt(Instant.now()).user(User.builder().id(1L).login("tester").build()));
     }
 
     public static Inventory getInventoryRandomSampleGenerator() {
